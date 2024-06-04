7f60ce2f0537648daec54b6cd6f1a6045dcd9272
jamesbrobb <jamesbenrobb@gmail.com>
2024-06-04 22:50:34 +0100

    added chart sync directive

---
+++ library/src/lib/chart-sync/chart-sync.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ChartSyncDirective } from './chart-sync.directive';
+
+describe('ChartSyncDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ChartSyncDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ library/src/lib/chart-sync/chart-sync.directive.ts
@@ -0,0 +1,11 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[tvChartSync]',
+  standalone: true
+})
+export class TVChartSyncDirective {
+
+  constructor() { }
+
+}
