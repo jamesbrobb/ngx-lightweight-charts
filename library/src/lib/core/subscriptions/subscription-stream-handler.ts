473a7e57bd5d5f838ade8974e98acbcda187a7f8
jamesbrobb <jamesbenrobb@gmail.com>
2024-06-12 22:56:59 +0100

    refactored subscription handler removing wrapper functions for handlers

--- library/src/lib/core/subscriptions/subscription-stream-handler.ts
+++ library/src/lib/core/subscriptions/subscription-stream-handler.ts
@@ -1,4 +1,4 @@
-import {BehaviorSubject, fromEventPattern, share, Subject, switchMap, takeUntil} from "rxjs";
+import {BehaviorSubject, fromEventPattern, map, Observable, share, Subject, switchMap, takeUntil, tap} from "rxjs";
 
 
 export type HandlerCallbackFn<T> = (arg: T) => void;
@@ -7,37 +7,26 @@ export type SubHandlerFn<T> = (handler: HandlerCallbackFn<T>) => void
 
 export class SubscriptionStreamHandler<Param> {
 
-  readonly #subscribeFn: SubHandlerFn<Param>;
-  readonly #unsubscribeFn: SubHandlerFn<Param>;
-
   readonly #subject = new BehaviorSubject<Param | undefined>(undefined);
 
   readonly #destroy = new Subject();
   readonly #destroy$ = this.#destroy.asObservable();
 
-  readonly #subscribeHandler = (handler: HandlerCallbackFn<Param>): void => {
-    this.#subscribeFn(handler);
-  }
-
-  readonly #unsubscribeHandler = (handler: HandlerCallbackFn<Param>): void => {
-    this.#unsubscribeFn(handler);
-  }
-
-  readonly stream$ = this.#subject.asObservable().pipe(
-    switchMap(() => fromEventPattern<Param>(
-      this.#subscribeHandler,
-      this.#unsubscribeHandler
-    )),
-    takeUntil(this.#destroy$),
-    share()
-  );
+  readonly stream$: Observable<Param>;
 
   constructor(
     subscribeFn: SubHandlerFn<Param>,
     unsubscribeFn: SubHandlerFn<Param>
   ) {
-    this.#subscribeFn = subscribeFn;
-    this.#unsubscribeFn = unsubscribeFn;
+    this.stream$ = this.#subject.asObservable().pipe(
+      switchMap(() => fromEventPattern<any[]>(
+        subscribeFn,
+        unsubscribeFn
+      )),
+      map<any[], Param>((args: any[]) => args[0]),
+      takeUntil(this.#destroy$),
+      share()
+    );
   }
 
   destroy(): void {
