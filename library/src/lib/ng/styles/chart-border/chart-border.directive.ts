d1b85312c0b124c7a2474461a3b526bd354c3abc
jamesbrobb <jamesbenrobb@gmail.com>
2024-06-13 13:48:02 +0100

    updated border style directive to accept input of options object rather than individual inputs

--- library/src/lib/ng/styles/chart-border/chart-border.directive.ts
+++ library/src/lib/ng/styles/chart-border/chart-border.directive.ts
@@ -1,16 +1,24 @@
 import {Directive, effect, inject, input} from '@angular/core';
 import {TVChartCollectorDirective} from "../../chart-collector/chart-collector.directive";
+import {TVChart} from "../../../core";
+import {SeriesType} from "lightweight-charts";
+
+
+export type ChartBorderStyles = {
+  borderColor?: string
+  borderVisible?: boolean
+}
 
 
 @Directive({
   selector: '[tvChartBorder]',
   standalone: true
 })
-export class TVChartBorderDirective {
+export class TVChartBorderDirective<T extends SeriesType, HorzItemScale> {
 
-  borderColor = input<string>('#555');
+  borderStyles = input.required<ChartBorderStyles>({alias: 'tvChartBorder'});
 
-  #collector = inject(TVChartCollectorDirective);
+  readonly #collector = inject(TVChartCollectorDirective<T, HorzItemScale>);
 
   constructor() {
     effect(() => {
@@ -21,21 +29,37 @@ export class TVChartBorderDirective {
       }
 
       charts.forEach(chart => {
-        chart.applyOptions(this.#getStyles());
+        const styles = this.#getStyles(chart);
+
+        if(!styles) {
+          return;
+        }
+
+        chart.applyOptions(styles);
       })
     });
   }
 
-  #getStyles() {
+  #getStyles(chart: TVChart<T, HorzItemScale>) {
+    const borderStyles = this.borderStyles(),
+      current = chart.options;
+
+    if(!current) {
+      return;
+    }
+
     return {
       timeScale: {
-        borderColor: this.borderColor()
+        borderColor: borderStyles.borderColor || current.timeScale?.borderColor,
+        borderVisible: borderStyles.borderVisible || current.timeScale?.borderVisible
       },
       leftPriceScale: {
-        borderColor: this.borderColor()
+        borderColor: borderStyles.borderColor || current.leftPriceScale?.borderColor,
+        borderVisible: borderStyles.borderVisible || current.leftPriceScale?.borderVisible
       },
       rightPriceScale: {
-        borderColor: this.borderColor()
+        borderColor: borderStyles.borderColor || current.rightPriceScale?.borderColor,
+        borderVisible: borderStyles.borderVisible || current.rightPriceScale?.borderVisible
       }
     }
   }
