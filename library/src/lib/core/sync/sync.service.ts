b42ee98b2cffc1f85eec8c3926f31ef7fe76106c
jamesbrobb <jamesbenrobb@gmail.com>
2024-06-14 17:31:14 +0100

    completed implementing sync service - now syncs visible logical range and crosshair position of supplied charts

--- library/src/lib/core/sync/sync.service.ts
+++ library/src/lib/core/sync/sync.service.ts
@@ -1,32 +1,166 @@
-import {ISeriesApi, LogicalRange, Range, SeriesType, Time} from "lightweight-charts";
-import {Observable} from "rxjs";
+import {LogicalRange, MouseEventParams, Point, Range} from "lightweight-charts";
+import {BehaviorSubject, filter, map, merge, Observable, share, Subject, Subscription, takeUntil, tap} from "rxjs";
 
 
-export interface Syncable<HorzScaleItem = Time> {
+export type Syncable = {
   setVisibleLogicalRange(range: Range<number>): void
-  //setVisibleRange(range: Range<HorzScaleItem>): void
-  //setCrosshairPosition(price: number, horizontalPosition: HorzScaleItem, seriesApi: ISeriesApi<SeriesType, HorzScaleItem>): void
-  //clearCrosshairPosition(): void
-  visibleLogicalRangeChange$: Observable<LogicalRange | null>
+  readonly visibleLogicalRangeChange$: Observable<LogicalRange | null>
+}
+
+export type SyncableWithCrosshair<HorzScaleItem> = Syncable & {
+  setCrossHairPositionByPoint(point: Point): void
+  clearCrosshairPosition(): void
+  readonly crossHairMove$: Observable<MouseEventParams<HorzScaleItem>>
+}
+
+export function isSyncableWithCrosshair<HorzScaleItem>(arg: Syncable): arg is SyncableWithCrosshair<HorzScaleItem> {
+  return 'setCrosshairPosition' in arg &&
+    'clearCrosshairPosition' in arg &&
+    'crossHairMove$' in arg;
+}
+
+export function isOutputWithData<T>(update: MultiStreamOutput<T> | undefined): update is MultiStreamOutput<NonNullable<T>> {
+  return !!update && !!update.data;
 }
 
 
-export class SyncService {
+export class SyncService<HorzScaleItem> {
 
-  readonly #syncables: Syncable[] = [];
+  #syncables?: Syncable[];
 
-  register(arg: Syncable): void {
-    if(this.#syncables.includes(arg)) {
-      return;
+  readonly #destroy = new Subject<void>();
+  readonly #destroyed$ = this.#destroy.asObservable();
+
+  readonly #visibleLogicalRange = new MultiStream<LogicalRange | null>();
+  readonly visibleLogicalRange$ = this.#visibleLogicalRange.stream$.pipe(
+    filter(isOutputWithData<LogicalRange | null>),
+    tap(arg => {
+      this.#syncables
+        ?.filter(
+          syncable => syncable.visibleLogicalRangeChange$ !== arg?.source
+        )
+        .forEach(syncable => {
+          syncable.setVisibleLogicalRange(arg.data);
+        });
+    }),
+    map(arg => arg.data),
+    share()
+  );
+
+  readonly #crosshairPosition = new MultiStream<MouseEventParams<HorzScaleItem>>();
+  readonly crosshairPosition$ = this.#crosshairPosition.stream$.pipe(
+    filter((update): update is MultiStreamOutput<MouseEventParams<HorzScaleItem>> => !!update && !!update.data),
+    tap(arg => {
+      this.#syncables
+        ?.filter(isSyncableWithCrosshair<HorzScaleItem>)
+        .filter(syncable => syncable.crossHairMove$ !== arg?.source)
+        .forEach(syncable => {
+          const data = arg.data;
+
+          if (!data.logical || !data.point || !data.time) {
+            syncable.clearCrosshairPosition();
+            return;
+          }
+
+          syncable.setCrossHairPositionByPoint(data.point);
+        });
+    }),
+    map(arg => arg?.data),
+    share()
+  );
+
+  constructor() {
+    this.visibleLogicalRange$.pipe(
+      takeUntil(this.#destroyed$),
+    ).subscribe();
+
+    this.crosshairPosition$.pipe(
+      takeUntil(this.#destroyed$),
+    ).subscribe();
+  }
+
+  register(arg: Syncable[], clearExisting: boolean = true): void {
+
+    if(clearExisting || !this.#syncables) {
+      this.#syncables = [];
     }
-    this.#syncables.push(arg);
+
+    arg = arg.filter(arg => !this.#syncables!.includes(arg));
+
+    this.#syncables = this.#syncables.concat(arg);
+
+    this.#updateStreams();
   }
 
   deregister(arg: Syncable): void {
+
+    if(!this.#syncables) {
+      return;
+    }
+
     const index = this.#syncables.indexOf(arg);
+
     if(index === -1) {
       return;
     }
+
     this.#syncables.splice(index, 1);
+    this.#updateStreams();
+  }
+
+  #updateStreams(): void {
+
+    if(!this.#syncables) {
+      return;
+    }
+
+    this.#visibleLogicalRange.updateObservables(
+      this.#syncables.map(syncable => syncable.visibleLogicalRangeChange$)
+    );
+
+    this.#crosshairPosition.updateObservables(
+      this.#syncables.filter(isSyncableWithCrosshair<HorzScaleItem>)
+        .map(syncable => syncable.crossHairMove$)
+    );
+  }
+
+  destroy(): void {
+    this.#visibleLogicalRange.destroy();
+    this.#crosshairPosition.destroy();
+    this.#destroy.next();
+    this.#destroy.complete();
+  }
+}
+
+
+export type MultiStreamOutput<T> = {
+  source: Observable<T>,
+  data: T
+}
+
+
+export class MultiStream<T> {
+
+  readonly #subject = new BehaviorSubject<MultiStreamOutput<T> | undefined>(undefined);
+  readonly stream$ = this.#subject.asObservable();
+
+  #subscription?: Subscription;
+
+  updateObservables(streams: Observable<T>[]): void {
+
+    this.#cleanUp();
+
+    this.#subscription = merge(...streams.map(arg => arg.pipe(
+      map(data => ({source: arg, data}))
+    ))).subscribe(this.#subject);
+  }
+
+  destroy(): void {
+    this.#cleanUp();
+  }
+
+  #cleanUp(): void {
+    this.#subscription?.unsubscribe();
+    this.#subscription = undefined;
   }
 }
